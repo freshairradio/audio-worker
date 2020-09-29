c4c01fc0a8beeba5cce874ad005ce6e6a94df0cb
Samuel Macleod <samuel@macleod.space>
2020-09-29 02:42:21 +0100

    With new deployment

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,70 @@
-import crypto from 'crypto'
-import axios from 'axios'
-import aws from 'aws-sdk'
-import md5 from 'md5'
-import ffmpeg from 'fluent-ffmpeg'
-import fs from 'fs'
-import express from 'express'
-const port = process.env.PORT
-import { getAudioDurationInSeconds } from 'get-audio-duration'
+import crypto from "crypto";
+import axios from "axios";
+import aws from "aws-sdk";
+import md5 from "md5";
+import { spawn, exec } from "child_process";
+import fetch from "node-fetch";
+import fs from "fs";
+import express from "express";
+import { config } from "dotenv";
+config();
+const port = process.env.PORT;
+
 const s3 = new aws.S3({
-  endpoint: 'nyc3.digitaloceanspaces.com',
+  endpoint: "nyc3.digitaloceanspaces.com",
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY
-})
+});
+const ffprobe = (file) =>
+  new Promise((yes, no) => {
+    let data = "";
+    spawn("sh", [
+      "-c",
+      `ffprobe -i ${file} -show_entries format=duration -v quiet -of csv="p=0"`
+    ])
+      .stdout.on("data", (d) => (data += d))
+      .on("end", () => yes(parseInt(data)));
+  });
 const processAudio = (url) => {
-  console.log('Starting to process media:', url)
+  console.log("Starting to process media:", url);
   return new Promise(async (yes, no) => {
-    const fileName = crypto.randomBytes(20).toString('hex')
-    console.log('Assigned random filename:', fileName)
-    console.log('Beginning media download')
+    const fileName = crypto.randomBytes(20).toString("hex");
+    console.log("Assigned random filename:", fileName);
+    console.log("Beginning media download");
     const data = (
       await axios.get(url, {
-        responseType: 'arraybuffer'
+        responseType: "arraybuffer"
       })
-    ).data
-    await fs.promises.writeFile(`/tmp/${fileName}i`, data)
-    console.log('Media downloaded')
-    console.log('Beginning ffmpeg process')
+    ).data;
+    await fs.promises.writeFile(`/tmp/${fileName}i`, data);
+    console.log("Media downloaded");
+    console.log("Beginning ffmpeg process");
 
-    ffmpeg(`/tmp/${fileName}i`)
-      .format('mp3')
-      .audioBitrate(128)
-      .save(`/tmp/${fileName}`)
-      .on('progress', (progress) => {
-        console.log('Processing: ' + Math.round(progress.percent) + '% done')
+    const ffmpeg = spawn("ffmpeg", [
+      "-i",
+      `/tmp/${fileName}i`,
+      `/tmp/${fileName}.mp3`
+    ]);
+    ffmpeg.stderr
+      .on("data", (data) => {
+        console.log(data.toString());
       })
 
-      .on('error', console.error)
-      .on('end', async () => {
-        console.log('ffmpeg process finished')
+      .on("error", console.error)
+      .on("end", async () => {
+        console.log("ffmpeg process finished");
 
-        console.log('Starting Spaces upload')
-        const data = await fs.promises.readFile(`/tmp/${fileName}`)
-        const length = await getAudioDurationInSeconds(`/tmp/${fileName}`)
-        const hash = md5(data)
+        console.log("Starting Spaces upload");
+        const data = await fs.promises.readFile(`/tmp/${fileName}.mp3`);
+        const length = await ffprobe(`/tmp/${fileName}.mp3`);
+        console.log(length);
+        const hash = md5(data);
         let params = {
-          Bucket: 'freshair',
-          ACL: 'public-read',
-          ContentType: 'audio/mpeg'
-        }
+          Bucket: "freshair",
+          ACL: "public-read",
+          ContentType: "audio/mpeg"
+        };
+        console.log("Hash:", hash);
         // let existing = await s3
         //   .listObjectsV2({
         //     Bucket: 'freshair',
@@ -59,7 +76,7 @@ const processAudio = (url) => {
         //     audio: `https://cdn.freshair.dev/processed_media/mp3/${hash}`
         //   })
         // }
-        const dataStream = fs.createReadStream(`/tmp/${fileName}`)
+        const dataStream = fs.createReadStream(`/tmp/${fileName}.mp3`);
 
         await s3
           .putObject({
@@ -67,38 +84,51 @@ const processAudio = (url) => {
             Body: dataStream,
             Key: `processed_media/mp3/${hash}`
           })
-          .on('httpUploadProgress', (event) => {
+          .on("httpUploadProgress", (event) => {
             console.log(
               `Uploaded ${Math.round((event.loaded / event.total) * 100)}%`
-            )
+            );
           })
-          .promise()
+          .promise();
         // await uploadToZetta(data, name)
         yes({
           audio: `https://cdn.freshair.radio/processed_media/mp3/${hash}`,
           length
-        })
-      })
-  })
-}
+        });
+      });
+  });
+};
 
-const app = express()
-app.use(express.json())
-
+const app = express();
+app.use(express.json());
+app.use(async (req, res, next) => {
+  const user = await fetch(`https://identity.freshair.radio/user`, {
+    headers: {
+      authorization: req.headers.authorization
+    }
+  });
+  if (user.status != 200) {
+    return res.status(401).json({ error: "Please provide a valid auth token" });
+  }
+  let json = await user.json();
+  req.user = json;
+  return next();
+});
 app.post(`/process`, (req, res) => {
   processAudio(req.body.audio).then((r: object) => {
-    console.log(r)
+    console.log(r);
     axios.put(
       req.body.update_url,
-      { meta: { ...r, published: true } },
+      { ...r },
       {
         headers: {
-          Authorization: process.env.SHARED_API_SECRET,
-          'Content-Type': 'application/json'
+          Authorization: req.headers.authorization,
+          "Content-Type": "application/json"
         }
       }
-    )
-  })
-  return res.status(200).send()
-})
-app.listen(port)
+    );
+  });
+  return res.status(200).send();
+});
+console.log("Listening");
+app.listen(port, () => "Server started");
